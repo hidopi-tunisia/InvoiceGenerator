41bd4fe88d92a067cae46cc18a00f72400c2ca85
hamdi.chebbi <hamdi.chebbi@gmail.com>
2025-04-16 17:30:49 +0000

    auth

---
+++ app/(auth)/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TestComponent = () => (
+  <View>
+    <Text>This is a test component</Text>
+  </View>
+);
+
+export default TestComponent;
--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,18 @@
-export const VEXO_API_KEY= process.env.EXPO_PUBLIC_VEXO_API_KEY || '4277a15f-8ec3-4fdc-ad1c-e6e2f5c61c40'
+// app/config.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// Importez d'autres services Firebase si nécessaire (Firestore, etc.)
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  // measurementId: "YOUR_MEASUREMENT_ID" // Facultatif
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+// Exportez d'autres instances de service si nécessaire
