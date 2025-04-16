91e5e5efff6fc6e3632bd1e71eae600be94cc33f
hidopi-tunisia <h.chebbi@hidopi.com>
2025-04-16 19:08:36 +0200

    ajout de la gestion des destinataires dans le composant Home ; mise à jour de l'endpoint et du token d'authentification

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,28 @@
 import { Stack, router } from 'expo-router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { customEvent } from 'vexo-analytics';
 
 import { Button } from '~/components/Button';
+import { createRecipient, getRecipients } from '~/domain/recipients';
+import { createSender } from '~/domain/senders';
 import { useStore } from '~/store';
 
 export default function Home() {
+  const [loadingCreate, setLoadingCreate] = useState(false);
+  const [errorCreate, setErrorCreate] = useState('');
+  const [loadingDelete, setLoadingDelete] = useState(false);
+  const [errorDelete, setErrorDelete] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [vat, setVAT] = useState('');
   // Reprendre les facture aprés fermeture de l'application
   const existingNewInvoice = useStore((data) => data.newInvoice);
   const startNewInvoice = useStore((data) => data.startNewInvoice);
+
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const onNewInvoice = () => {
     startNewInvoice();
@@ -21,7 +34,51 @@ export default function Home() {
     customEvent('Reprendre Fatoura', {});
     router.push('/invoices/generate');
   };
+  const retrieve = async () => {
+    try {
+      setLoading(true);
+      setError('');
+      const { data } = await getRecipients({
+        page: 1,
+        limit: 20,
+        /**
+            name,
+            email
+          */
+      });
+      console.log('data', data);
 
+      setItems(data);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      setError('Error happened');
+      console.log(e);
+    }
+  };
+  const handleOnPress = () => {
+    const fn = async () => {
+      try {
+        setLoadingCreate(true);
+        setErrorCreate('');
+        await createRecipient({
+          name: 'Morchka',
+          address: 'Tunis',
+          email: 'morchka@wabna.com',
+          vat: 20,
+        });
+        setLoadingCreate(false);
+        setName('');
+        setAddress('');
+        retrieve();
+      } catch (e) {
+        setLoadingCreate(false);
+        setErrorCreate('Error happened');
+        console.log(e);
+      }
+    };
+    fn();
+  };
   return (
     <>
       <Stack.Screen options={{ title: 'Accueil', headerShown: false }} />
@@ -47,6 +104,7 @@ export default function Home() {
           <Button variant="link" title="Reprendre la Facture" onPress={onResumeInvoice} />
         )}
       </View>
+      <Button variant="link" title="Recipients" onPress={handleOnPress} />
     </>
   );
 }
